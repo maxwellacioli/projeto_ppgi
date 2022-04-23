7a5e325b471a790e297ec870019c6d27ae65b408
Maxwell Acioli <maxwell.acioli@gmail.com>
2022-04-23 10:54:07 -0300

    add new dataset analisys

--- root/meta_analysis.py
+++ root/meta_analysis.py
@@ -15,9 +15,9 @@ from sklearn import preprocessing
 import seaborn as sns
 
 
-df_0 = pd.read_csv("metadata1.csv", sep=';')
-df_1 = pd.read_csv("metadata2.csv", sep=';')
-df = pd.read_csv("metadata3.csv", sep=';')
+# df_0 = pd.read_csv("metadata1.csv", sep=';')
+# df_1 = pd.read_csv("metadata2.csv", sep=';')
+df = pd.read_csv("metadata100.csv", sep=';')
 
 df_selected_feats = pd.read_csv("selected_features_data.csv", sep=',')
 columns_names_selected_feats = list(df_selected_feats)
@@ -28,115 +28,115 @@ columns_names_selected_feats = list(df_selected_feats)
 
 #### NEW DATA ####
 
-df = df.append(df_0, ignore_index=True)
-df = df.append(df_1, ignore_index=True)
+# df = df.append(df_0, ignore_index=True)
+# df = df.append(df_1, ignore_index=True)
 
 columns_names = list(df)
 
 
-#### ACCURACY ####
+# #### ACCURACY ####
 
-sns.distplot(df['time_acc_rf_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
+# sns.distplot(df['time_acc_rf_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
 
-sns.distplot(df['time_acc_rf_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
+# sns.distplot(df['time_acc_rf_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
 
-plt.title('Density Accuracy Plot RF')
-plt.legend(prop={'size': 10})
-plt.show()
+# plt.title('Density Accuracy Plot RF')
+# plt.legend(prop={'size': 10})
+# plt.show()
 
-sns.distplot(df['time_acc_gnb_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
+# sns.distplot(df['time_acc_gnb_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
 
-sns.distplot(df['time_acc_gnb_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
+# sns.distplot(df['time_acc_gnb_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
 
-plt.title('Density Accuracy Plot GNB')
-plt.legend(prop={'size': 10})
-plt.show()
+# plt.title('Density Accuracy Plot GNB')
+# plt.legend(prop={'size': 10})
+# plt.show()
 
-sns.distplot(df['time_acc_lr_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
+# sns.distplot(df['time_acc_dt_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
 
-sns.distplot(df['time_acc_lr_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
+# sns.distplot(df['time_acc_dt_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
 
-plt.title('Density Accuracy Plot LR')
-plt.legend(prop={'size': 10})
-plt.show()
+# plt.title('Density Accuracy Plot dt')
+# plt.legend(prop={'size': 10})
+# plt.show()
 
-sns.distplot(df['time_acc_neight_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
+# sns.distplot(df['time_acc_neight_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
 
-sns.distplot(df['time_acc_neight_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
+# sns.distplot(df['time_acc_neight_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
 
-plt.title('Density Accuracy Plot KNN')
-plt.legend(prop={'size': 10})
-plt.show()
-
-
-#### PRECISION ####
-
-sns.distplot(df['time_prec_rf_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
-
-sns.distplot(df['time_prec_rf_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
-
-plt.title('Density Precision Plot RF')
-plt.legend(prop={'size': 10})
-plt.show()
-
-sns.distplot(df['time_prec_gnb_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
-
-sns.distplot(df['time_prec_gnb_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
-
-plt.title('Density Precision Plot GNB')
-plt.legend(prop={'size': 10})
-plt.show()
-
-sns.distplot(df['time_prec_lr_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
-
-sns.distplot(df['time_prec_lr_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
-
-plt.title('Density Precision Plot LR')
-plt.legend(prop={'size': 10})
-plt.show()
-
-sns.distplot(df['time_prec_neight_original_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'original_feats', color='red')
-
-sns.distplot(df['time_prec_neight_selected_feats'], hist = False, kde = True,
-                  kde_kws = {'shade': False, 'linewidth': 1}, 
-                  label = 'selected_feats', color='blue')
-
-plt.title('Density Precision Plot KNN')
-plt.legend(prop={'size': 10})
-plt.show()
+# plt.title('Density Accuracy Plot KNN')
+# plt.legend(prop={'size': 10})
+# plt.show()
 
 
-boxplot = df.boxplot(column=['time_acc_lr_original_feats', 'time_acc_lr_selected_feats'])
+# #### PRECISION ####
+
+# sns.distplot(df['time_prec_rf_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
+
+# sns.distplot(df['time_prec_rf_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
+
+# plt.title('Density Precision Plot RF')
+# plt.legend(prop={'size': 10})
+# plt.show()
+
+# sns.distplot(df['time_prec_gnb_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
+
+# sns.distplot(df['time_prec_gnb_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
+
+# plt.title('Density Precision Plot GNB')
+# plt.legend(prop={'size': 10})
+# plt.show()
+
+# sns.distplot(df['time_prec_dt_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
+
+# sns.distplot(df['time_prec_dt_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
+
+# plt.title('Density Precision Plot dt')
+# plt.legend(prop={'size': 10})
+# plt.show()
+
+# sns.distplot(df['time_prec_neight_original_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'original_feats', color='red')
+
+# sns.distplot(df['time_prec_neight_selected_feats'], hist = False, kde = True,
+#                   kde_kws = {'shade': False, 'linewidth': 1}, 
+#                   label = 'selected_feats', color='blue')
+
+# plt.title('Density Precision Plot KNN')
+# plt.legend(prop={'size': 10})
+# plt.show()
+
+
+boxplot = df.boxplot(column=['time_acc_dt_original_feats', 'time_acc_dt_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_acc_neight_original_feats', 'time_acc_neight_selected_feats'])
 plt.show()
@@ -144,8 +144,12 @@ boxplot = df.boxplot(column=['time_acc_gnb_original_feats', 'time_acc_gnb_select
 plt.show()
 boxplot = df.boxplot(column=['time_acc_rf_original_feats', 'time_acc_rf_selected_feats'])
 plt.show()
+boxplot = df.boxplot(column=['time_acc_mlp_original_feats', 'time_acc_mlp_selected_feats'])
+plt.show()
+boxplot = df.boxplot(column=['time_acc_svc_original_feats', 'time_acc_svc_selected_feats'])
+plt.show()
 
-boxplot = df.boxplot(column=['time_prec_lr_original_feats', 'time_prec_lr_selected_feats'])
+boxplot = df.boxplot(column=['time_prec_dt_original_feats', 'time_prec_dt_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_prec_neight_original_feats', 'time_prec_neight_selected_feats'])
 plt.show()
@@ -153,8 +157,12 @@ boxplot = df.boxplot(column=['time_prec_gnb_original_feats', 'time_prec_gnb_sele
 plt.show()
 boxplot = df.boxplot(column=['time_prec_rf_original_feats', 'time_prec_rf_selected_feats'])
 plt.show()
+boxplot = df.boxplot(column=['time_prec_mlp_original_feats', 'time_prec_mlp_selected_feats'])
+plt.show()
+boxplot = df.boxplot(column=['time_prec_svc_original_feats', 'time_prec_svc_selected_feats'])
+plt.show()
 
-boxplot = df.boxplot(column=['time_rec_lr_original_feats', 'time_rec_lr_selected_feats'])
+boxplot = df.boxplot(column=['time_rec_dt_original_feats', 'time_rec_dt_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_rec_neight_original_feats', 'time_rec_neight_selected_feats'])
 plt.show()
@@ -162,14 +170,22 @@ boxplot = df.boxplot(column=['time_rec_gnb_original_feats', 'time_rec_gnb_select
 plt.show()
 boxplot = df.boxplot(column=['time_rec_rf_original_feats', 'time_rec_rf_selected_feats'])
 plt.show()
+boxplot = df.boxplot(column=['time_rec_mlp_original_feats', 'time_rec_mlp_selected_feats'])
+plt.show()
+boxplot = df.boxplot(column=['time_rec_svc_original_feats', 'time_rec_svc_selected_feats'])
+plt.show()
 
-boxplot = df.boxplot(column=['time_f1_lr_original_feats', 'time_f1_lr_selected_feats'])
+boxplot = df.boxplot(column=['time_f1_dt_original_feats', 'time_f1_dt_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_f1_neight_original_feats', 'time_f1_neight_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_f1_gnb_original_feats', 'time_f1_gnb_selected_feats'])
 plt.show()
 boxplot = df.boxplot(column=['time_f1_rf_original_feats', 'time_f1_rf_selected_feats'])
+plt.show()
+boxplot = df.boxplot(column=['time_f1_mlp_original_feats', 'time_f1_mlp_selected_feats'])
+plt.show()
+boxplot = df.boxplot(column=['time_f1_svc_original_feats', 'time_f1_svc_selected_feats'])
 plt.show()
 
 
--- root/proj.py
+++ root/proj.py
@@ -11,15 +11,21 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from time import time
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 
 labelEncoder = LabelEncoder()
 
-lr = LogisticRegression(random_state=50,max_iter=10000)
+mlp = MLPClassifier(random_state=1, max_iter=300)
+svc = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+dt = DecisionTreeClassifier(random_state=0)
 neigh = KNeighborsClassifier(n_neighbors=25, leaf_size=10)
 gnb = GaussianNB()
 rf = RandomForestClassifier(criterion='entropy', max_depth= 7, class_weight='balanced', random_state=42)
@@ -30,7 +36,7 @@ df_treated = pd.read_csv("metabric_treated_data.csv", low_memory=False)
 
 df_treated = df_treated.iloc[: , 1:]
 
-target0 = df_treated.pop('type_of_breast_surgery')
+target0 = df_treated.pop('overall_survival')
 
 df_treated_column_names = list(df_treated)
 
@@ -52,61 +58,103 @@ for column in df_treated_selected_column_names:
 
 ###################################
 
-columns_metadata_names = ["time_acc_lr_original_feats",
-                "time_acc_lr_selected_feats",
+columns_metadata_names = ["time_acc_dt_original_feats",
+                "time_acc_dt_selected_feats",
                 "time_acc_neight_original_feats",
                 "time_acc_neight_selected_feats",
                 "time_acc_gnb_original_feats",
                 "time_acc_gnb_selected_feats",
                 "time_acc_rf_original_feats",
                 "time_acc_rf_selected_feats",
-                "time_prec_lr_original_feats",
-                "time_prec_lr_selected_feats",
+                "time_acc_mlp_original_feats",
+                "time_acc_mlp_selected_feats",
+                "time_acc_svc_original_feats",
+                "time_acc_svc_selected_feats"
+                "time_prec_dt_original_feats",
+                "time_prec_dt_selected_feats",
                 "time_prec_neight_original_feats",
                 "time_prec_neight_selected_feats",
                 "time_prec_gnb_original_feats",
                 "time_prec_gnb_selected_feats",
                 "time_prec_rf_original_feats",
                 "time_prec_rf_selected_feats",
-                "time_rec_lr_original_feats",
-                "time_rec_lr_selected_feats",
+                "time_prec_mlp_original_feats",
+                "time_prec_mlp_selected_feats",
+                "time_prec_svc_original_feats",
+                "time_prec_svc_selected_feats"
+                "time_rec_dt_original_feats",
+                "time_rec_dt_selected_feats",
                 "time_rec_neight_original_feats",
                 "time_rec_neight_selected_feats",
                 "time_rec_gnb_original_feats",
                 "time_rec_gnb_selected_feats",
                 "time_rec_rf_original_feats",
                 "time_rec_rf_selected_feats",
-                "time_f1_lr_original_feats",
-                "time_f1_lr_selected_feats",
+                "time_rec_mlp_original_feats",
+                "time_rec_mlp_selected_feats",
+                "time_rec_svc_original_feats",
+                "time_rec_svc_selected_feats"
+                "time_f1_dt_original_feats",
+                "time_f1_dt_selected_feats",
                 "time_f1_neight_original_feats",
                 "time_f1_neight_selected_feats",
                 "time_f1_gnb_original_feats",
                 "time_f1_gnb_selected_feats",
                 "time_f1_rf_original_feats",
-                "time_f1_rf_selected_feats"]
+                "time_f1_rf_selected_feats",
+                "time_f1_mlp_original_feats",
+                "time_f1_mlp_selected_feats",
+                "time_f1_svc_original_feats",
+                "time_f1_svc_selected_feats"
+                ]
 
 metadata_df = pd.DataFrame(columns = columns_metadata_names)
 
-index = 0
-while index < 1:
+index = 1
+while index <=100:
+    print('current index: ', index)
     ## Accuracy ##
     
-    # lr #
+     # svc #
     
-    clf = lr
+    clf = svc
     
     t = time()
-    acc_lr_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='accuracy'))
-    time_acc_lr_original_feats = round(time()-t,3)
+    acc_svc_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='accuracy'))
+    time_acc_svc_original_feats = round(time()-t,3)
 
     t = time()
-    acc_lr_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='accuracy'))
-    time_acc_lr_selected_feats = round(time()-t,3)
+    acc_svc_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='accuracy'))
+    time_acc_svc_selected_feats = round(time()-t,3)
+    
+     # mlp #
+    
+    clf = mlp
+    
+    t = time()
+    acc_mlp_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='accuracy'))
+    time_acc_mlp_original_feats = round(time()-t,3)
+
+    t = time()
+    acc_mlp_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='accuracy'))
+    time_acc_mlp_selected_feats = round(time()-t,3)
+    
+    # dt #
+    
+    clf = dt
+    
+    t = time()
+    acc_dt_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='accuracy'))
+    time_acc_dt_original_feats = round(time()-t,3)
+
+    t = time()
+    acc_dt_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='accuracy'))
+    time_acc_dt_selected_feats = round(time()-t,3)
     
     # knn#
     
     clf = neigh
-    
+
     t = time()
     acc_neight_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='accuracy'))
     time_acc_neight_original_feats = round(time()-t,3)
@@ -141,17 +189,42 @@ while index < 1:
     
     ## Precision ##
     
-    # lr #
+    # svc #
+    
+    clf = svc
+    
+    t = time()
+    prec_svc_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='precision'))
+    time_prec_svc_original_feats = round(time()-t,3)
 
-    clf = lr
+    t = time()
+    prec_svc_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='precision'))
+    time_prec_svc_selected_feats = round(time()-t,3)
+    
+     # mlp #
+    
+    clf = mlp
     
     t = time()
-    prec_lr_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='precision'))
-    time_prec_lr_original_feats = round(time()-t,3)
+    prec_mlp_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='precision'))
+    time_prec_mlp_original_feats = round(time()-t,3)
+
+    t = time()
+    prec_mlp_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='precision'))
+    time_prec_mlp_selected_feats = round(time()-t,3)
+    
+    
+    # dt #
+
+    clf = dt
     
     t = time()
-    prec_lr_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='precision'))
-    time_prec_lr_selected_feats = round(time()-t,3)
+    prec_dt_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='precision'))
+    time_prec_dt_original_feats = round(time()-t,3)
+    
+    t = time()
+    prec_dt_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='precision'))
+    time_prec_dt_selected_feats = round(time()-t,3)
     
     # knn#
     
@@ -191,17 +264,42 @@ while index < 1:
     
     ## Recall ##
     
-    # lr #
+    # svc #
+    
+    clf = svc
+    
+    t = time()
+    rec_svc_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='recall'))
+    time_rec_svc_original_feats = round(time()-t,3)
 
-    clf = lr
+    t = time()
+    rec_svc_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='recall'))
+    time_rec_svc_selected_feats = round(time()-t,3)
+    
+     # mlp #
+    
+    clf = mlp
     
     t = time()
-    rec_lr_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='recall'))
-    time_rec_lr_original_feats = round(time()-t,3)
+    rec_mlp_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='recall'))
+    time_rec_mlp_original_feats = round(time()-t,3)
+
+    t = time()
+    rec_mlp_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='recall'))
+    time_rec_mlp_selected_feats = round(time()-t,3)
+    
+    
+    # dt #
+
+    clf = dt
     
     t = time()
-    rec_lr_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='recall'))
-    time_rec_lr_selected_feats = round(time()-t,3)
+    rec_dt_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='recall'))
+    time_rec_dt_original_feats = round(time()-t,3)
+    
+    t = time()
+    rec_dt_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='recall'))
+    time_rec_dt_selected_feats = round(time()-t,3)
     
     # knn#
     
@@ -241,17 +339,42 @@ while index < 1:
     
     ## F1 ##
     
-    # lr #
+    # svc #
+    
+    clf = svc
+    
+    t = time()
+    f1_svc_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='f1'))
+    time_f1_svc_original_feats = round(time()-t,3)
 
-    clf = lr
+    t = time()
+    f1_svc_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='f1'))
+    time_f1_svc_selected_feats = round(time()-t,3)
+    
+     # mlp #
+    
+    clf = mlp
     
     t = time()
-    f1_lr_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='f1'))
-    time_f1_lr_original_feats = round(time()-t,3)
+    f1_mlp_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='f1'))
+    time_f1_mlp_original_feats = round(time()-t,3)
+
+    t = time()
+    f1_mlp_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='f1'))
+    time_f1_mlp_selected_feats = round(time()-t,3)
+    
+    
+    # dt #
+
+    clf = dt
     
     t = time()
-    f1_lr_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='f1'))
-    time_f1_lr_selected_feats = round(time()-t,3)
+    f1_dt_original_feats = np.mean(cross_val_score(clf, df_treated, target0, cv=10, scoring='f1'))
+    time_f1_dt_original_feats = round(time()-t,3)
+    
+    t = time()
+    f1_dt_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='f1'))
+    time_f1_dt_selected_feats = round(time()-t,3)
     
     # knn#
     
@@ -289,41 +412,58 @@ while index < 1:
     f1_rf_selected_feats = np.mean(cross_val_score(clf, df_treated_selected, target1, cv=10, scoring='f1'))
     time_f1_rf_selected_feats = round(time()-t,3)
     
-    new_data = {"time_acc_lr_original_feats": time_acc_lr_original_feats, 
-                "time_acc_lr_selected_feats": time_acc_lr_selected_feats,
+    new_data = {"time_acc_dt_original_feats": time_acc_dt_original_feats, 
+                "time_acc_dt_selected_feats": time_acc_dt_selected_feats,
                 "time_acc_neight_original_feats": time_acc_neight_original_feats,
                 "time_acc_neight_selected_feats": time_acc_neight_selected_feats,
                 "time_acc_gnb_original_feats": time_acc_gnb_original_feats,
                 "time_acc_gnb_selected_feats": time_acc_gnb_selected_feats,
                 "time_acc_rf_original_feats": time_acc_rf_original_feats,
                 "time_acc_rf_selected_feats": time_acc_rf_selected_feats,
-                "time_prec_lr_original_feats": time_prec_lr_original_feats,
-                "time_prec_lr_selected_feats": time_prec_lr_selected_feats,
+                "time_acc_mlp_original_feats": time_acc_mlp_original_feats,
+                "time_acc_mlp_selected_feats": time_acc_mlp_selected_feats,
+                "time_acc_svc_original_feats": time_acc_svc_original_feats,
+                "time_acc_svc_selected_feats": time_acc_svc_selected_feats,
+                "time_prec_dt_original_feats": time_prec_dt_original_feats,
+                "time_prec_dt_selected_feats": time_prec_dt_selected_feats,
                 "time_prec_neight_original_feats": time_prec_neight_original_feats,
                 "time_prec_neight_selected_feats": time_prec_neight_selected_feats,
                 "time_prec_gnb_original_feats": time_prec_gnb_original_feats,
                 "time_prec_gnb_selected_feats": time_prec_gnb_selected_feats,
                 "time_prec_rf_original_feats": time_prec_rf_original_feats,
                 "time_prec_rf_selected_feats": time_prec_rf_selected_feats,
-                "time_rec_lr_original_feats": time_rec_lr_original_feats,
-                "time_rec_lr_selected_feats": time_rec_lr_selected_feats,
+                "time_prec_mlp_original_feats": time_prec_mlp_original_feats,
+                "time_prec_mlp_selected_feats": time_prec_mlp_selected_feats,
+                "time_prec_svc_original_feats": time_prec_svc_original_feats,
+                "time_prec_svc_selected_feats": time_prec_svc_selected_feats,
+                "time_rec_dt_original_feats": time_rec_dt_original_feats,
+                "time_rec_dt_selected_feats": time_rec_dt_selected_feats,
                 "time_rec_neight_original_feats": time_rec_neight_original_feats,
                 "time_rec_neight_selected_feats": time_rec_neight_selected_feats,
                 "time_rec_gnb_original_feats": time_rec_gnb_original_feats,
                 "time_rec_gnb_selected_feats": time_rec_gnb_selected_feats,
                 "time_rec_rf_original_feats": time_rec_rf_original_feats,
                 "time_rec_rf_selected_feats": time_rec_rf_selected_feats,
-                "time_f1_lr_original_feats": time_f1_lr_original_feats,
-                "time_f1_lr_selected_feats": time_f1_lr_selected_feats,
+                "time_rec_mlp_original_feats": time_rec_mlp_original_feats,
+                "time_rec_mlp_selected_feats": time_rec_mlp_selected_feats,
+                "time_rec_svc_original_feats": time_rec_svc_original_feats,
+                "time_rec_svc_selected_feats": time_rec_svc_selected_feats,
+                "time_f1_dt_original_feats": time_f1_dt_original_feats,
+                "time_f1_dt_selected_feats": time_f1_dt_selected_feats,
                 "time_f1_neight_original_feats": time_f1_neight_original_feats,
                 "time_f1_neight_selected_feats": time_f1_neight_selected_feats,
                 "time_f1_gnb_original_feats": time_f1_gnb_original_feats,
                 "time_f1_gnb_selected_feats": time_f1_gnb_selected_feats,
                 "time_f1_rf_original_feats": time_f1_rf_original_feats,
-                "time_f1_rf_selected_feats": time_f1_rf_selected_feats}
+                "time_f1_rf_selected_feats": time_f1_rf_selected_feats,
+                "time_f1_mlp_original_feats": time_f1_mlp_original_feats,
+                "time_f1_mlp_selected_feats": time_f1_mlp_selected_feats,
+                "time_f1_svc_original_feats": time_f1_svc_original_feats,
+                "time_f1_svc_selected_feats": time_f1_svc_selected_feats,
+                }
     
     metadata_df = metadata_df.append(new_data, ignore_index=True)
     
     index+=1
 
-metadata_df.to_csv('metadata9.csv', sep=';', encoding='utf-8', index=False)
+metadata_df.to_csv('metadata104.csv', sep=';', encoding='utf-8', index=False)
